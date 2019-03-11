eecae3279ff1176ab135053ff97e95b902b439c2
Alexander Brown <alex.brown.f@gmail.com>
2019-03-11 16:37:57 -0500

    working on 2nd parts

--- aliens/static/js/app.js
+++ aliens/static/js/app.js
@@ -13,13 +13,11 @@ tableData.forEach((ufoSighting) => {
 
 submit.on("click", function() {
     d3.event.preventDefault();
-    d3.select('#ufo-table').selectAll("td").remove()
-    var inputElement = d3.select("#datetime");
+    d3.select('#ufo-table').selectAll("td").remove();
+    var cityElement 
+    var inputElement = d3.select("#datetime"); //this needs to loop through the form and find the values 
     var inputValue = inputElement.property("value");
-    console.log(inputValue);
-    console.log(tableData);
     var filteredData = tableData.filter(date => date.datetime === inputValue );
-    console.log(filteredData)
     filteredData.forEach((ufoSighting) => {
         var row = tbody.append("tr");
         Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -29,8 +27,6 @@ submit.on("click", function() {
       });
 });
 
-
-
 // Input field formatter!!!
 // var text = d3.select("#datetime");
 // function handleChange(event) {
@@ -38,3 +34,25 @@ submit.on("click", function() {
 //     output.html("");
 // }
 // YOUR CODE HERE!
+
+
+//needs handleChange as an event listener 
+
+// WORKING CODE HERE - DONT DELETE TILL ABOVE IS ALLLLLL DONE
+// submit.on("click", function() {
+//     d3.event.preventDefault();
+//     d3.select('#ufo-table').selectAll("td").remove()
+//     var inputElement = d3.select("#datetime"); //this needs to loop through the form and find the values 
+//     var inputValue = inputElement.property("value");
+//     console.log(inputValue);
+//     console.log(tableData);
+//     var filteredData = tableData.filter(date => date.datetime === inputValue );
+//     console.log(filteredData)
+//     filteredData.forEach((ufoSighting) => {
+//         var row = tbody.append("tr");
+//         Object.entries(ufoSighting).forEach(([key, value]) => {
+//           var cell = tbody.append("td");
+//           cell.text(value);
+//         });
+//       });
+// });
